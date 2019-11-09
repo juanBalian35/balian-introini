9af0cdf1f302b97c61e699c3795c7acd082c815f
agustin <agustinintro1127@gmail.com>
2019-11-09 04:42:48 -0300

    Primer avance significante en la interfaz principal

--- src/ecoshop/Main.java
+++ src/ecoshop/Main.java
@@ -4,7 +4,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
@@ -17,8 +19,11 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/ecoshop/frontend/Interfaz.fxml"));
         
-        Scene scene = new Scene(root);
+        stage.initStyle(StageStyle.TRANSPARENT);
         
+        Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+       
         stage.setScene(scene);
         stage.show();
     }
--- src/ecoshop/frontend/InterfazController.java
+++ src/ecoshop/frontend/InterfazController.java
@@ -2,7 +2,12 @@ package ecoshop.frontend;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 /**
  *
@@ -14,5 +19,52 @@ public class InterfazController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+  
+
+    @FXML
+    private void close(MouseEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
+    }
+
+     double x, y;
+     
+    @FXML
+    private void dragged(MouseEvent event) {
+         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+         stage.setX(event.getScreenX() - x);
+         stage.setY(event.getScreenY() - y);
+    }
+
+    @FXML
+    private void pressed(MouseEvent event) {
+        x=event.getSceneX();
+        y=event.getSceneY();
+    }
+
+    @FXML
+    private void click_btn1(MouseEvent event) {
+        
+    }
+
+    @FXML
+    private void click_btn2(MouseEvent event) {
+    }
+
+    @FXML
+    private void click_btn3(MouseEvent event) {
+    }
+
+    @FXML
+    private void click_btn4(MouseEvent event) {
+    }
+
+    @FXML
+    private void click_btn5(MouseEvent event) {
+    }
+
+    @FXML
+    private void click_btn6(MouseEvent event) {
+    }
     
 }
