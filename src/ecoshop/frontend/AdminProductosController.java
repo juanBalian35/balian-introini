65c1eef00e250d51d932883234007d52ea6f6397
agustinintro11 <agustinintro_123@hotmail.com>
2019-11-18 17:03:06 -0300

    iconos agregados, signo de peso funcionando

--- src/ecoshop/frontend/AdminProductosController.java
+++ src/ecoshop/frontend/AdminProductosController.java
@@ -8,6 +8,8 @@ package ecoshop.frontend;
 import com.jfoenix.controls.*;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.UnaryOperator;
+import java.util.regex.Pattern;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
@@ -15,6 +17,15 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Side;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.control.TextFormatter.Change;
+
+import javafx.scene.layout.Pane;
+import javafx.util.converter.IntegerStringConverter;
 
 /*
  * FXML Controller class
@@ -31,6 +42,43 @@ public class AdminProductosController implements Initializable {
     
     @FXML
     private JFXTextField TFBuscar;
+    
+    @FXML
+    private JFXTextField TBPrecio;
+    
+    @FXML
+    private Label signoPeso;
+    
+    @FXML
+    private Pane pane;
+    
+    boolean controlSignoPeso = false;
+       UnaryOperator<TextFormatter.Change> filter = new UnaryOperator<TextFormatter.Change>() {
+
+            @Override
+            public TextFormatter.Change apply(TextFormatter.Change t) {
+
+                if (t.isReplaced()) 
+                    if(t.getText().matches("[^0-9]"))
+                        t.setText(t.getControlText().substring(t.getRangeStart(), t.getRangeEnd()));
+                
+
+                if (t.isAdded()) {
+                    if (t.getControlText().contains(".")) {
+                        if (t.getText().matches("[^0-9]")) {
+                            t.setText("");
+                        }
+                    } else if (t.getText().matches("[^0-9.]")) {
+                        t.setText("");
+                    }
+                }
+
+                return t;
+            }
+        };
+    
+    
+    
     /**
      * Initializes the controller class.
      * @param url
@@ -38,21 +86,60 @@ public class AdminProductosController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+      TBPrecio.setTextFormatter(new TextFormatter<>(filter));    
       TFBuscar.textProperty().addListener(new ChangeListener<String>() {
         @Override
         public void changed(ObservableValue<? extends String> observable,
                 String oldValue, String newValue) {
             botonBuscar.disableProperty().setValue(newValue.length() == 0);
         }
+        
+        
+    });
+      TBPrecio.textProperty().addListener(new ChangeListener<String>() {
+        @Override
+        public void changed(ObservableValue<? extends String> observable,
+                String oldValue, String newValue) {
+                if(!controlSignoPeso && newValue.length()<=3){
+                    TBPrecio.setText(oldValue);
+                }
+                controlSignoPeso = false;
+        }
+        
+        
     });
       
-    }    
-    
-    @FXML
-    private  void accionBoxBuscarPor(ActionEvent event) {
+      //TFBuscar.isFocused()
+     TBPrecio.focusedProperty().addListener(new ChangeListener<Boolean>() {
+        @Override
+        public void changed(ObservableValue<? extends Boolean> observable,
+                Boolean wasFocused, Boolean isNowFocused) {
+            if(isNowFocused){
+                signoPeso.setVisible(true);
+                if(!(TBPrecio.getText().length()>3)){
+                    TBPrecio.setText("    ");
+                    
+                }
+            }
+            else if(TBPrecio.getText().length() == 4){
+                signoPeso.setVisible(false);
+                controlSignoPeso = true;
+                TBPrecio.setText("");
+            }
+        } 
+    });   
+     
+     
+     
+     
+    }
+ 
+     @FXML
+       private  void accionBoxBuscarPor(ActionEvent event) {
        Object seleccion = BoxBuscarPor.getValue();
        TFBuscar.disableProperty().setValue(Boolean.FALSE);
        TFBuscar.promptTextProperty().setValue((String) seleccion);
 
     }
+  
 }
