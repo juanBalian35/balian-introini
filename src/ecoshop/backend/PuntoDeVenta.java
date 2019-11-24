5f3fc64367952ada8eaefa6bd96fb7349ec4639a
juanBalian35 <juanchi_b_r@hotmail.com>
2019-11-24 02:53:25 -0300

    Puntos de venta se ven en su tableViewBorrar

--- src/ecoshop/backend/PuntoDeVenta.java
+++ src/ecoshop/backend/PuntoDeVenta.java
@@ -1,25 +1,59 @@
 package ecoshop.backend;
 
+import java.util.Map;
+import java.util.Set;
+
 /**
  *
  * @author Agustín Introini
  * @author Juan Balian
  */
 public class PuntoDeVenta {
-    private String direccion;
+    private int id;
     private String nombre;
-
-    public PuntoDeVenta(String direccion, String nombre) {
-        this.direccion = direccion;
-        this.nombre = nombre;
+    private String departamento;
+    private String ciudad;
+    private String calle;
+    private String numero;
+    
+    public int getId() {
+        return id;
     }
 
-    public String getDireccion() {
-        return direccion;
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
     }
 
     public String getNombre() {
@@ -30,4 +64,37 @@ public class PuntoDeVenta {
         this.nombre = nombre;
     }
     
+    public String getDireccion(){
+        return calle + " " + numero + ", " + ciudad + ", " + departamento;
+    }
+    
+    public static PuntoDeVenta parsearEntrySet(Set<Map.Entry<String, String>> entrySet){
+        PuntoDeVenta puntoDeVenta = new PuntoDeVenta();
+        for(Map.Entry<String,String> entry : entrySet){
+            switch(entry.getKey().toLowerCase()){
+                case "nombre":
+                    puntoDeVenta.setNombre(entry.getValue());
+                    break;
+                case "id":
+                    puntoDeVenta.setId(Integer.parseInt(entry.getValue()));
+                    break;
+                case "calle":
+                    puntoDeVenta.setCalle(entry.getValue());
+                    break;
+                case "departamento":
+                    puntoDeVenta.setDepartamento(entry.getValue());
+                    break; 
+                case "ciudad":
+                    puntoDeVenta.setCiudad(entry.getValue());
+                    break; 
+                case "numero":
+                    puntoDeVenta.setNumero(entry.getValue());
+                    break; 
+                default:
+                    throw new IllegalArgumentException("Argumento invalido en entry set");
+            }
+        }
+        
+        return puntoDeVenta;
+    }
 }
--- src/ecoshop/frontend/PuntosDeVentaController.java
+++ src/ecoshop/frontend/PuntosDeVentaController.java
@@ -5,18 +5,37 @@
  */
 package ecoshop.frontend;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import ecoshop.backend.Envase;
+import ecoshop.backend.JSONAuxiliar;
+import ecoshop.backend.PuntoDeVenta;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+import javafx.application.Platform;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableObjectValue;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Worker.State;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebEvent;
 import javafx.scene.web.WebView;
+import org.json.simple.JSONObject;
 
 /**
  * FXML Controller class
@@ -24,24 +43,51 @@ import javafx.scene.web.WebView;
  * @author agustinintroini
  */
 public class PuntosDeVentaController implements Initializable {
-    @FXML
-    private WebView webView;
+    private static final String NOMBRE_JSON = "puntosDeVenta";
     
-    @FXML
-    private Pane paneWebView;
+    @FXML private WebView webView;
+    
+    @FXML private Pane paneWebView;
+    
+    @FXML private JFXTextField TFCalle;
+    @FXML private JFXTextField TFNumero;
+    @FXML private JFXTextField TFId;
+    @FXML private JFXTextField TFNombre;
+    @FXML private JFXTextField TFCiudad;
+    @FXML private JFXComboBox BoxDepartamentos;
+
+    @FXML private TableView tableViewBorrar;
+    @FXML private TableColumn<PuntoDeVenta, String> columnId;
+    @FXML private TableColumn<PuntoDeVenta, String> columnNombre;
+    @FXML private TableColumn<PuntoDeVenta, String> columnDireccion;
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        columnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        columnNombre.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+        columnDireccion.setCellValueFactory(features -> new SimpleStringProperty(features.getValue().getDireccion()));
+        
         MyBrowser myBrowser = new MyBrowser();
         
         paneWebView.getChildren().add(myBrowser);
     }    
     
+    private void actualizarDatos(){
+        ArrayList<PuntoDeVenta> puntos = 
+                JSONAuxiliar.procesarArchivo(NOMBRE_JSON, PuntoDeVenta::parsearEntrySet);
+        for(int i = 0 ; i < puntos.size(); ++i){
+            agregarMarcador(puntos.get(i).getCalle(), puntos.get(i).getNumero(), 
+                    puntos.get(i).getCiudad(), puntos.get(i).getDepartamento());
+        }
+        //validadorRepeticionId.setExistentes((ArrayList<String>)(envases.stream().map(x -> x.getId() + "").collect(Collectors.toList())));
+        tableViewBorrar.getItems().setAll(puntos);
+    }
+    
     class MyBrowser extends Pane {
         WebEngine webEngine = webView.getEngine();
-
 
         public MyBrowser() {
             final URL urlGoogleMaps = getClass().getResource("demo.html");
@@ -54,30 +100,41 @@ public class PuntosDeVentaController implements Initializable {
             });
 
             getChildren().add(webView);
-
-            Button update = new Button("Update");
-            update.setOnAction(new EventHandler<ActionEvent>() {
-
-                @Override
-                public void handle(ActionEvent arg0) {
-                    //lat = Double.parseDouble(latitude.getText());
-                    //lon = Double.parseDouble(longitude.getText());
-                    /*
-                    System.out.printf("%.2f %.2f%n", lat, lon);
-
-                    webEngine.executeScript("" +
-                        "window.lat = " + lat + ";" +
-                        "window.lon = " + lon + ";" +
-                        "document.goToLocation(window.lat, window.lon);"
-                    );*/
+            webEngine.getLoadWorker().stateProperty().addListener(new ChangeListener<State>() {
+                public void changed(@SuppressWarnings("rawtypes") ObservableValue ov, State oldState, State newState){
+                    System.out.println(newState);
+                    actualizarDatos();  
                 }
             });
-
-            HBox toolbar  = new HBox();
-            //toolbar.getChildren().addAll(latitude, longitude, update);
-
-            getChildren().addAll(toolbar);
         }
+    }
+    
+    @FXML
+    private void agregarPuntoDeVenta(ActionEvent event){
+        String departamento = ((Label)BoxDepartamentos.getValue()).getText();
+        
+        agregarMarcador(TFCalle.getText(), TFNumero.getText(), TFCiudad.getText(), departamento);
+                
+        JSONObject puntoDeVenta = new JSONObject();
+        puntoDeVenta.put("id", TFId.getText());
+        puntoDeVenta.put("nombre", TFNombre.getText());
+        puntoDeVenta.put("departamento", departamento);
+        puntoDeVenta.put("ciudad", TFCiudad.getText());
+        puntoDeVenta.put("calle", TFCalle.getText());
+        puntoDeVenta.put("numero", TFNumero.getText());
+        
+        JSONAuxiliar.agregar(puntoDeVenta, NOMBRE_JSON);
+    }
+    
+    private void agregarMarcador(String calle, String numero, 
+            String ciudad, String departamento){
+        String direccion = calle + " " + numero + ", " + ciudad 
+                + ", " + departamento;
+        
+        webView.getEngine().executeScript("" +
+            "window.direccion = '" + direccion + "';" +
+            "document.goToLocation(window.direccion);"
+        );
     }
 
 }
