0d869b687b9b58be95d42c01b65f36948d5f0474
juanBalian35 <juanchi_b_r@hotmail.com>
2019-11-25 00:55:54 -0300

    Se guardan pdf

--- src/ecoshop/frontend/RegistrarVentaController.java
+++ src/ecoshop/frontend/RegistrarVentaController.java
@@ -13,8 +13,17 @@ import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXDialog;
 import com.jfoenix.controls.JFXDialogLayout;
 import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.base.IFXLabelFloatControl;
+import com.jfoenix.validation.NumberValidator;
+import com.jfoenix.validation.RequiredFieldValidator;
+import com.jfoenix.validation.base.ValidatorBase;
+import com.lowagie.text.pdf.PdfWriter;
 import ecoshop.backend.JSONAuxiliar;
 import ecoshop.backend.Producto;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 import javafx.beans.property.SimpleStringProperty;
@@ -26,12 +35,24 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
+import org.apache.fontbox.ttf.TrueTypeCollection;
+import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.PDResources;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType0Font;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-
+import org.apache.fontbox.ttf.TrueTypeFont;
 
 /**
  * FXML Controller class
@@ -55,13 +76,17 @@ public class RegistrarVentaController implements Initializable {
     @FXML private Pane paneDetallesVenta;
     @FXML private JFXButton botonBuscar;
     
-    @FXML TableView<Producto> tableViewProductos;
-    @FXML private TableColumn<Producto, String> columnId1;
-    @FXML private TableColumn<Producto, String> columnNombre1;
-    @FXML private TableColumn<Producto, String> columnPrecioUnit;
-    @FXML private TableColumn<Producto, String> columnEnvases1;
-    @FXML private TableColumn<Producto, String> columnCantidad;
-    @FXML private TableColumn<Producto, String> columnPrecioTotal;
+    @FXML TableView<C> tableViewProductos;
+    @FXML private TableColumn<C, String> columnId1;
+    @FXML private TableColumn<C, String> columnNombre1;
+    @FXML private TableColumn<C, String> columnPrecioUnit;
+    @FXML private TableColumn<C, String> columnEnvases1;
+    @FXML private TableColumn<C, String> columnCantidad;
+    @FXML private TableColumn<C, String> columnPrecioTotal;
+    
+    @FXML private JFXTextField TFTotal;
+    @FXML private JFXTextField TFEmail;
+    @FXML private JFXTextField TFNombre;
     
     /**
      * Initializes the controller class.
@@ -89,12 +114,46 @@ public class RegistrarVentaController implements Initializable {
         columnNombre1.setCellValueFactory(new PropertyValueFactory<>("nombre"));
         columnPrecioUnit.setCellValueFactory(new PropertyValueFactory<>("precio"));
         columnEnvases1.setCellValueFactory(new PropertyValueFactory<>("envases"));
-        //columnCantidad.setCellValueFactory(new PropertyValueFactory<>("cantidad"));
+        columnCantidad.setCellValueFactory(new PropertyValueFactory<>("cantidad"));
         columnPrecioTotal.setCellValueFactory(new PropertyValueFactory<>("precioTotal"));
-        //columnCantidad.setCellValueFactory(features -> new SimpleStringProperty(features.getValue().getDireccion()));
+        columnCantidad.setCellValueFactory(new PropertyValueFactory<>("cantidad"));
         
+        validarCampo(TFEmail, 
+               new String[]{"Campo obligatorio"}, 
+               new ValidatorBase[]{new RequiredFieldValidator()});
+        validarCampo(TFNombre, 
+               new String[]{"Campo obligatorio"}, 
+               new ValidatorBase[]{new RequiredFieldValidator()});
         actualizarDatos();
     }    
+    
+    private void validarCampo(IFXLabelFloatControl campo, 
+            String[] mensajes,
+            ValidatorBase[] validators){
+        for(int i = 0; i < validators.length; ++i){
+            validators[i].setMessage(mensajes[i]);
+            campo.getValidators().add(validators[i]);
+            
+            Image image = new Image(getClass().getResourceAsStream("recursos/attention.png"));
+            ImageView icono = new ImageView(image);
+            icono.setFitHeight(13);
+            icono.setFitWidth(13);
+            validators[i].setIcon(icono);
+        }
+      
+        if(campo instanceof JFXTextField){
+            ((JFXTextField)campo).focusedProperty().addListener((o, oldVal, newVal) -> {
+                if (!newVal && campo.validate())
+                    ((JFXTextField)campo).getStyleClass().add("error");
+            });
+        }
+        else if(campo instanceof JFXComboBox){
+            ((JFXComboBox)campo).focusedProperty().addListener((o, oldVal, newVal) -> {
+                if (!newVal && campo.validate())
+                    ((JFXComboBox)campo).getStyleClass().add("error");
+            });
+        }
+    }
     
     private void actualizarDatos(){
         ArrayList<Producto> productos = 
@@ -132,19 +191,83 @@ public class RegistrarVentaController implements Initializable {
         tableViewBuscar.getItems().setAll(productos);
     }
     
+    ArrayList<C> ces = new ArrayList<>();
+    
     @FXML
     private void clickBotonAgregarProducto(ActionEvent event){
         paneDetallesVenta.setDisable(false);
         
         Producto producto = tableViewBuscar.getSelectionModel().getSelectedItem();
-        tableViewProductos.getItems().add(producto);
+
+        boolean yaExistia = false;
+        for(int i = 0 ; i < ces.size(); ++i){
+            if(ces.get(i).id == producto.getId()){
+                ces.get(i).setCantidad(ces.get(i).cantidad + 1);
+                yaExistia = true;
+            }
+        }
+        
+        if(!yaExistia){
+            C c = new C(producto.getId(),producto.getPrecio(),producto.getMaterial(),
+                producto.getDescripcion(),producto.getImagen(),producto.getNombre(),
+                producto.getEnvases());
+            ces.add(c);
+        }
+        
+        double total = this.ces.stream().mapToDouble(x -> x.precioTotal).sum();
+        
+        TFTotal.setText(total + "");
+        tableViewProductos.getItems().setAll(ces);
     }
+    
+    
     
     @FXML
-    void botonConfirmarCompra(ActionEvent event) {
+    void clickBotonConfirmarCompra(MouseEvent event)  {
+        boolean emailValido = TFEmail.validate();
+        boolean nombreValido = TFNombre.validate();
+        if(!(emailValido && nombreValido)){
+            return;
+        }
+        
+        String dest = "a.pdf";
+
+        try{
+            PDDocument document = new PDDocument();
+            PDPage page = new PDPage();
+            document.addPage( page );
+
+            // Create a new font object selecting one of the PDF base fonts
+            //PDFont font = PDType1Font.TIMES_BOLD;
+            // Start a new content stream which will "hold" the to be created content
+            PDPageContentStream contentStream = new PDPageContentStream(document, page);
+            
+            InputStream starWarsFontStream = RegistrarVentaController.class.getResourceAsStream("rob.ttf");
+            PDType0Font font = PDType0Font.load(document, starWarsFontStream, true);
+            //URI ruta = getClass().getResource("\\estilos\\fuentes").toURI();
+            //System.out.println(ruta);
+            //PDType0Font font = PDType0Font.load(document, new File(ruta));
+
+            // Define a text content stream using the selected font, moving the cursor and drawing the text "Hello World"
+            contentStream.beginText();
+            contentStream.setFont( font, 12 );
+            contentStream.moveTextPositionByAmount( 100, 700 );
+            contentStream.drawString( "Hello World" );
+            contentStream.endText();
+
+            // Make sure that the content stream is closed:
+            contentStream.close();
+
+            // Save the results and ensure that the document is properly closed:
+            document.save( "Hello World.pdf");
+            document.close();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
     }
     
-    /*private class C{
+    public class C{
         private int id;
         private double precio;
         private String material;
@@ -153,7 +276,93 @@ public class RegistrarVentaController implements Initializable {
         private String nombre;
         private ArrayList<Integer> envases;
         private int cantidad;
+        private double precioTotal;
         
+        public C(int id, double precio, String material, String descripcion,
+                String imagen,String nombre, ArrayList<Integer> envases){
+            this.id = id;
+            this.precio = precio;
+            this.material = material;
+            this.descripcion = descripcion;
+            this.imagen = imagen;
+            this.nombre = nombre;
+            this.envases = envases;
+            
+            this.cantidad = 1;
+            this.precioTotal = precio;
+        }
         
-    }*/
+        public void setCantidad(int c){
+            this.cantidad = c;
+            this.precioTotal = precio * c;
+        }
+        
+        public int getCantidad(){
+            return this.cantidad;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public double getPrecio() {
+            return precio;
+        }
+
+        public void setPrecio(double precio) {
+            this.precio = precio;
+        }
+
+        public String getMaterial() {
+            return material;
+        }
+
+        public void setMaterial(String material) {
+            this.material = material;
+        }
+
+        public String getDescripcion() {
+            return descripcion;
+        }
+
+        public void setDescripcion(String descripcion) {
+            this.descripcion = descripcion;
+        }
+
+        public String getImagen() {
+            return imagen;
+        }
+
+        public void setImagen(String imagen) {
+            this.imagen = imagen;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public ArrayList<Integer> getEnvases() {
+            return envases;
+        }
+
+        public void setEnvases(ArrayList<Integer> envases) {
+            this.envases = envases;
+        }
+
+        public double getPrecioTotal() {
+            return precioTotal;
+        }
+
+        public void setPrecioTotal(double precioTotal) {
+            this.precioTotal = precioTotal;
+        }
+    }
 }
